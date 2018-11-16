7c4bda25f2096aa214754da9a5dc8c8ab9c9724b
Anna <anutaprok@mail.ru>
2018-11-16 22:08:20 +0300

    page info for admin

--- frontend/build/public/js/user_main.js
+++ frontend/build/public/js/user_main.js
@@ -15,3 +15,38 @@ $('.name h4').click(function() {
         $(this).parent().find("block_2").hide(200);
     }
 });
+
+function getUser() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', '/home', true);
+
+    xhr.onload = function () {
+        data = JSON.parse(xhr.responseText);
+        document.getElementsById("username").innerHTML = data.LastName + " " + data.FirstName + " " + data.Patronymic;
+        document.getElementsByTagName("faculty").innerHTML = data.Faculty;
+        document.getElementsByTagName("course").innerHTML = data.Course;
+        document.getElementsByTagName("ave_ball").innerHTML = data.AverageMark;
+    };
+    xhr.send();
+};
+
+function getAch() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('POST', '/home', true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onload = function () {
+        data = JSON.parse(xhr.responseText);
+        if(data.curr) {                             //Если достижение - текущее, записываем его в текущие
+            document.getElementsByTagName("curr_criteria").innerHTML = data.crit;
+            document.getElementsByTagName("curr_desc").innerHTML = data.comment;
+            document.getElementsByTagName("curr_status").innerHTML = data.status;
+        }
+        else {
+            document.getElementsByTagName("arc_criteria").innerHTML = data.crit;
+            document.getElementsByTagName("arc_desc").innerHTML = data.comment;
+        }
+    };
+    xhr.send();
+};
